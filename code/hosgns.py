f609fb442c39ddd37d62917886fbbabfdd05e3d9
simonepiaggesi <piaggesi.simone@gmail.com>
2020-06-23 19:34:14 +0200

    Added license

--- code/hosgns.py
+++ code/hosgns.py
@@ -1,3 +1,25 @@
+# Copyright (C) 2020 Simone Piaggesi (Python3 script),
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+# The following guides provide helpful information if you want to extend this
+# script:
+#  http://nagiosplug.sourceforge.net/developer-guidelines.html (plug-in
+#  development guidelines)
+###############################################################################
+
 import tensorflow as tf
 from tensorflow.keras import Model
 import numpy as np
